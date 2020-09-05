eb5a7f22f03994577a95491362646d544ece3213
chugad <69381456+chugad@users.noreply.github.com>
2020-09-05 15:06:08 +0700

    Update StatsService.java

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -3,32 +3,28 @@ package ru.netology.stats;
 public class StatsService {
     /* Сумма всех продаж */
     public long calculateSumByYear(long[] salesByMonth) {
-        long SumByYear = 0;
-        long Index = 0;
+        long sumByYear = 0;
+        long index = 0;
         for (long sale : salesByMonth) {
-            SumByYear += sale;
-            Index = Index + 1;
+            sumByYear += sale;
+            index++;
         }
-        return SumByYear;
+        return sumByYear;
     }
     /* Средняя сумма всех продаж */
     public long calculateAverageSaleByYear(long[] salesByMonth) {
-        long Index = 0;
-        for (long sale : salesByMonth) {
-            Index = Index + 1;
-        }
         StatsService service = new StatsService();
-        long AverageSaleByYear = service.calculateSumByYear(salesByMonth) / Index;
-        return AverageSaleByYear;
+        long averageSaleByYear = service.calculateSumByYear(salesByMonth) / salesByMonth.length;
+        return averageSaleByYear;
     }
     /* Номер месяца в котором был пик продаж */
     public long showMonthMaxSales(long[] salesByMonth) {
-        long MaxSales = salesByMonth[0];
+        long maxSales = salesByMonth[0];
         long indexForMax = 0;
         for (int i = 0; i < salesByMonth.length; i++) {
             long sale = salesByMonth[i];
-            if (sale >= MaxSales) {
-                MaxSales = sale;
+            if (sale >= maxSales) {
+                maxSales = sale;
                 indexForMax = i + 1;
             }
         }
@@ -36,12 +32,12 @@ public class StatsService {
     }
     /* Номер месяца в котором был минимум продаж */
     public long showMonthMinSales(long[] salesByMonth) {
-        long MaxSales = salesByMonth[0];
+        long maxSales = salesByMonth[0];
         long indexForMax = 0;
         for (int i = 0; i < salesByMonth.length; i++) {
             long sale = salesByMonth[i];
-            if (sale <= MaxSales) {
-                MaxSales = sale;
+            if (sale <= maxSales) {
+                maxSales = sale;
                 indexForMax = i + 1;
             }
         }
@@ -49,25 +45,25 @@ public class StatsService {
     }
     /* Кол-во месяцев, в которых продажи были ниже среднего */
     public long calculateMonthsLowerAverageSales(long[] salesByMonth) {
-        long MonthsLowerAverageSales = 0;
+        long monthsLowerAverageSales = 0;
         StatsService service = new StatsService();
         for (long sale : salesByMonth) {
             if (sale < service.calculateAverageSaleByYear(salesByMonth)) {
-                MonthsLowerAverageSales = MonthsLowerAverageSales + 1;
+                monthsLowerAverageSales++;
             }
         }
-        return MonthsLowerAverageSales;
+        return monthsLowerAverageSales;
     }
     /* Кол-во месяцев, в которых продажи были выше среднего */
     public long calculateMonthsUpperAverageSales(long[] salesByMonth) {
-        long MonthsUpperAverageSales = 0;
+        long monthsUpperAverageSales = 0;
         StatsService service = new StatsService();
         for (long sale : salesByMonth) {
             if (sale > service.calculateAverageSaleByYear(salesByMonth)) {
-                MonthsUpperAverageSales = MonthsUpperAverageSales + 1;
+                monthsUpperAverageSales++;
             }
         }
-        return MonthsUpperAverageSales;
+        return monthsUpperAverageSales;
     }
 }
     /* Средняя сумма всех продаж первый вариант
